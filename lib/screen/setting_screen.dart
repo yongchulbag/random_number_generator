42976612666ed58f98a943cff576834041d50260
yongchulbag <flypeace@naver.com>
2024-05-12 19:05:46 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	lib/screen/home_screen.dart

--- lib/screen/setting_screen.dart
+++ lib/screen/setting_screen.dart
@@ -23,40 +23,70 @@ class _SettingsScreenState extends State<SettingsScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Expanded(
-                child: Row(
-                  children: maxNumber.toInt()
-                      .toString()
-                      .split('')
-                      .map((e) =>
-                          Image.asset('asset/img/$e.png', width: 30, height: 30))
-                      .toList(),
-                ),
-              ),
-              Slider(
-                value: maxNumber,
-                min: 1000,
-                max: 100000,
-                onChanged: (double val){
-                  print(val);
-                  setState(() {
-                    maxNumber=val;
-                  });
-                },
-              ),
-              ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pop(maxNumber.toInt());
-                },
-                style: ElevatedButton.styleFrom(
-                  primary: RED_COLOR,
-                ),
-                child: Text('저장'),
-              )
+              _Body(maxNumber: maxNumber),
+              _Footer(maxNumber: maxNumber, onSliderChanged: (double val){
+                print(val);
+                setState(() {
+                  maxNumber=val;
+                });
+              },onButtonPressed: () {
+                Navigator.of(context).pop(maxNumber.toInt());
+              },)
+              
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _Body extends StatelessWidget {
+  final double maxNumber;
+
+  const _Body({required this.maxNumber, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Expanded(
+      child: Row(
+        children: maxNumber.toInt()
+            .toString()
+            .split('')
+            .map((e) =>
+            Image.asset('asset/img/$e.png', width: 30, height: 30))
+            .toList(),
+      ),
+    );
+  }
+}
+
+class _Footer extends StatelessWidget {
+  final double maxNumber;
+  final ValueChanged<double>? onSliderChanged;
+  final VoidCallback onButtonPressed;
+
+  const _Footer({required this.maxNumber, required this.onSliderChanged, required this.onButtonPressed, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Slider(
+          value: maxNumber,
+          min: 1000,
+          max: 100000,
+          onChanged: onSliderChanged,
+        ),
+        ElevatedButton(
+          onPressed: onButtonPressed,
+          style: ElevatedButton.styleFrom(
+            primary: RED_COLOR,
+          ),
+          child: Text('저장'),
+        )
+      ],
     );
   }
 }
